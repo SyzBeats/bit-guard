95593b0f36a50ed6eef5263f60fe70ac70cf97f9
Simeon Zimmermann <syzdevacc@gmail.com>
2021-06-24 23:52:57 +0200

    feat (frontend): more components in dashboard

--- react-ui/src/components/styled/boxes/ContentBox.tsx
+++ react-ui/src/components/styled/boxes/ContentBox.tsx
@@ -8,15 +8,25 @@ interface ContextBoxInterface {
 const ContentBox = ({ title }: ContextBoxInterface) => {
   return (
     <Wrapper>
+      <ActionBar>
+        <ActionIcon background="red" />
+        <ActionIcon background="gold" />
+        <ActionIcon background="green" />
+      </ActionBar>
       <Title>{title}</Title>
     </Wrapper>
   );
+};
+
+type ActionIconType = {
+  background: string;
 };
 
 const Wrapper = styled.div`
   display: flex;
   justify-items: center;
   justify-content: flex-start;
+  flex-direction: column;
 
   border-radius: 0.25rem;
   box-shadow: ${({ theme }) => theme.shadows.depth_1};
@@ -24,6 +34,27 @@ const Wrapper = styled.div`
   margin: 2rem 1rem;
   padding: 1.5rem;
   height: 20rem;
+  background: ${({ theme }) => theme.colors.background};
+  color: white;
+`;
+
+const ActionBar = styled.nav`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 0 0 0.5rem 0;
+`;
+
+const ActionIcon = styled.div<ActionIconType>`
+  background: ${(props) => props.background};
+  height: 1.25rem;
+  width: 1.25rem;
+  border-radius: 50%;
+  margin: 0 0.5rem;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Title = styled.h2`
