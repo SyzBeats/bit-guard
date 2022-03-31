a466612959827e0a8bd4b6ee360799b9c473280d
Simeon Zimmermann <syzdevacc@gmail.com>
2022-03-31 23:55:14 +0200

    ft: cloud function included and packages updates

---
+++ gcp/cloud-functions/config.ts
@@ -0,0 +1,7 @@
+const config = {
+  HIT_LIMIT: 100,
+  TIMEOUT: 43200000,
+  PROJECT_ID: 'envite-341816',
+};
+
+export default config;
---
+++ gcp/cloud-functions/function.rate-limit.ts
@@ -0,0 +1,103 @@
+import { Firestore } from '@google-cloud/firestore';
+import config from './config';
+
+const db = new Firestore({ projectId: config.PROJECT_ID });
+const collection = db.collection('extension-store');
+
+exports.helloWorld = async (req, res) => {
+  // return if OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).send('OK');
+  }
+
+  // request body needs to have title and content
+  if (!req.body.title || !req.body.content) {
+    return res.status(400).send('Request body must contain title and content');
+  }
+
+  // fetch the document
+  const document = collection.doc(req.query.installToken);
+
+  if (!document) {
+    return res.status(400).send('Invalid install token');
+  }
+
+  const nowInMs = new Date().getTime();
+  const doc = await document.get();
+
+  // twelve hours in milliseconds
+
+  if (doc.exists) {
+    const data = doc.data();
+
+    if (!data) {
+      return res.status(500).send('Data does not exist');
+    }
+
+    const expirationInMs = new Date(data.expiration).getTime();
+    const hitCount = data.hitCount;
+
+    if (nowInMs > expirationInMs) {
+      // expired, user can hit the limit again
+      await resetUser(document, nowInMs);
+
+      await hitEnviteAPI();
+
+      return res.status(200).send('api was called');
+    }
+
+    if (hitCount >= config.HIT_LIMIT) {
+      return res.status(429).send('Too many requests');
+    }
+
+    await updateHitCount(document);
+
+    await hitEnviteAPI();
+
+    return res.send('Hello World!');
+  } else {
+    await setInitialDocument(document, nowInMs);
+  }
+};
+
+async function setInitialDocument(document, nowInMs) {
+  await document.set({
+    hitCount: 1,
+    expiration: new Date(nowInMs + config.TIMEOUT),
+  });
+}
+
+async function resetUser(document, nowInMs) {
+  await document.set({
+    expiration: nowInMs + config.TIMEOUT,
+    hitCount: 1,
+  });
+}
+
+async function updateHitCount(document) {
+  await document.update({
+    hitCount: document.hitCount + 1,
+  });
+}
+
+async function hitEnviteAPI() {
+  // call api with graphql query
+  // await fetch('https://api.graph.cool/simple/v1/cj7zf0zj0b7z70101xqjqjqj', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     query: `
+  //       query {
+  //         allUsers {
+  //           id
+  //           name
+  //           email
+  //         }
+  //       }
+  //     `,
+  //   }),
+  // });
+  return;
+}
