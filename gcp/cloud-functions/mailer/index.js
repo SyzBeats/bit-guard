7a2dc1e15da0ca585e44dbef7545c73bd5999047
Simeon Zimmermann <syzdevacc@gmail.com>
2022-09-18 13:13:42 +0200

    feat: added user register cloud function

--- gcp/cloud-functions/mailer/index.js
+++ gcp/cloud-functions/mailer/index.js
@@ -1,16 +1,17 @@
 const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 
-const SECRET_KEY = process.env.key || 'SomeSecret';
-const MAIL_USER = '';
-const MAIL_PASSWORD = '';
+const SECRET_KEY = process.env.KEY || 'SomeSecret';
+const MAIL_USER = process.env.MAIL_USER || 'mailuser';
+const MAIL_PASSWORD = process.env.MAIL_PASSWORD || 'mailpass';
+const REQ_AUTH = process.env.REQ_AUTH || 'strong-req-auth-pass';
 
 /**
  * Generates the email text.
- * @param data {object} the data of the registration
+ * @param data {{fullName:string, confirmationUrl: string}} the data of the registration
  */
 const generateText = (data) => `
- Hey ${data.name},
+ Hey ${data.fullName},
  
  Thank you for registering with https://envite.dev
  
@@ -35,15 +36,19 @@ exports.sendMail = async (req, res) => {
       return res?.status(200)?.send('OK');
     }
 
-    // get the current user ID - this will be sent from the main backend API alsong with username and email
+    // check if request is authorized
+    if (req.headers.authorization !== REQ_AUTH) {
+      return res?.status(401)?.send('Unauthorized');
+    }
 
-    const name = req.body.name;
+    // get the current user ID - this will be sent from the main backend API alsong with username and email
+    const fullName = `${req.body.firstName} ${req.body.lastName}`;
     const email = req.body.email;
     const id = req.body.id;
 
     // create a token
 
-    const token = jwt.sign({ id, email, name }, SECRET_KEY);
+    const token = jwt.sign({ id, email, name: fullName }, SECRET_KEY);
 
     // send an email to the user that has just registered and ask him to click the link
 
@@ -58,14 +63,14 @@ exports.sendMail = async (req, res) => {
     });
 
     const mailData = {
-      name,
-      confirmationUrl: `https://envite.dev/confirm?token=${token}`,
+      fullName,
+      confirmationUrl: `https://envite.dev/confirm/user?token=${token}`,
     };
 
-    await transporter.sendMail({
-      from: `"Envite" - <${MAIL_USER}>`,
+    return await transporter.sendMail({
+      from: `"Envite" <${MAIL_USER}>`,
       to: email,
-      subject: 'Confirm your registration',
+      subject: 'Please confirm your registration with .envite',
       text: generateText(mailData),
     });
   } catch (e) {
