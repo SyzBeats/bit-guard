9f9aa8f669505b9e3467e32cfa8d93d916d53d66
Simeon Zimmermann <syzdevacc@gmail.com>
2023-01-29 14:51:14 +0100

    feat(frontend): derive some file state from initial files and add new filereader to get base64 data

--- react-ui/src/components/ui/forms/inputs/ImageDropZone.tsx
+++ react-ui/src/components/ui/forms/inputs/ImageDropZone.tsx
@@ -7,21 +7,35 @@ const ImageDropZone = () => {
   // States
   const [file, setFile] = useState<File | undefined>(undefined);
 
-  const [selected, setSelected] = useState(false);
+  const [base64, setBase64] = useState<string | undefined>(undefined);
 
   // File selection handler
   const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
-      const files = Array.from(e.target.files);
-      setFile(files[0]);
-      setSelected(true);
+      const file = e.target.files[0];
+      // max 1024Kbit
+      if (file.size > 1024 * 1024) {
+        alert('File is too big! Max 1MB');
+        return;
+      }
+
+      setFile(file);
+
+      // create Buffer from file
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        setBase64(reader.result as string);
+      };
     }
   };
 
   // Class definitions
   const classes = [styles.dropzone];
 
-  if (selected) {
+  if (file) {
     classes.push(styles.selected);
   }
 
@@ -30,7 +44,7 @@ const ImageDropZone = () => {
     <div className={classes.join(' ')}>
       <label>
         <Image size="30" />
-        {file && <p>{file.name}</p>}
+        {file ? <p>{file.name}</p> : <p>Drop file here (Max 1024 Kb)</p>}
         <input type="file" id="file" accept="image/*" onChange={(e) => handleFile(e)} />
       </label>
     </div>
